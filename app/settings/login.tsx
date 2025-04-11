5c5985c6bc5f7d6d1e315caeba241edaa58db3df
NoorShbaro <12130557@students.liu.edu.lb>
2025-04-11 11:02:57 +0300

    isRTL

--- app/settings/login.tsx
+++ app/settings/login.tsx
@@ -72,7 +72,14 @@ export default function Login() {
                                 value={password}
                                 onChangeText={setPassword}
                             />
-                            <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)} style={[styles.eyeIcon, { right: isRTL ? 0 : 16, left: isRTL ? 16 : 0 }]}>
+                            <TouchableOpacity
+                                onPress={() => setPasswordVisible(!passwordVisible)}
+                                style={[
+                                    styles.eyeIcon,
+                                    isRTL ? { left: 16 } : { right: 16 }
+                                ]}
+                            >
+
                                 <MaterialIcons
                                     name={passwordVisible ? 'visibility' : 'visibility-off'}
                                     size={24}
